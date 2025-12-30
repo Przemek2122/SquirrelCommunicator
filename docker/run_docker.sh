42689f2a8fcdb8ac25a92a64fc8c914f609df75f
Przemek <przemek12wiewiora@gmail.com>
2025-12-30 20:35:05 +0100

    Add docker no cache param

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,4 +2,4 @@
 # run.sh → chmod +x run.sh && ./run.sh
 
 # --memory-swap is memory + swap so if we have 9GB Ram + 7 disk, its 9+7 = 16.
-docker run -it --rm --memory=9G --memory-swap=16G --cpus="1.5" squirrelcommunicator:latest /bin/bash
+docker run -it --rm --memory=9G --memory-swap=16G --cpus=2 squirrelcommunicator:latest /bin/bash
