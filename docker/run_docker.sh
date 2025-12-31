8ca5141213c6733b6e0205d651ccf98a13b73371
Przemek <przemek12wiewiora@gmail.com>
2025-12-31 15:00:21 +0100

    continous docker running

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # run.sh → chmod +x run.sh && ./run.sh
 
+# Run app docker until stopped
 # --memory-swap is memory + swap so if we have 9GB Ram + 7 disk, its 9+7 = 16.
-docker run -it --rm --memory=9G --memory-swap=16G --cpus=2 squirrelcommunicator:latest /bin/bash
+docker run -d --restart unless-stopped -p 8080-8081:8080-8081 -it --memory=9G --memory-swap=16G --cpus=2 squirrelcommunicator:latest /bin/bash
