d9f4ce9b7df4717f546da42d8a05b86e0a6b48a5
Przemek2122 <przemek12wiewiora@gmail.com>
2026-04-03 16:42:11 +0200

    Move DB Template to .env in docker

--- ProjectServer/TemplateSecrets/DatabaseTemplate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Database connections env vars
-export SQRLL_COMM_DB_HOST=127.0.0.1
-export SQRLL_COMM_DB_PORT=3306
-export SQRLL_COMM_DB_DBNAME=sqrllapitest
-export SQRLL_COMM_DB_USER=commapisqrllusertest
-export SQRLL_COMM_DB_PASSWORD=
