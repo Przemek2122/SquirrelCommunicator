3366b3756e0c1bc476a6eec1d7207960ebd7a78d
Przemek2122 <przemek12wiewiora@gmail.com>
2026-06-19 20:31:36 +0200

    Fix soci lib

--- ProjectServer/Source/Private/DataBase/DataBaseConnect.cpp
+++ ProjectServer/Source/Private/DataBase/DataBaseConnect.cpp
@@ -2,16 +2,16 @@
 #include "DataBase/DataBaseConnect.h"
 #include "DataBase/DataBaseSettings.h"
 #include "soci/session.h"
+#include "soci/mysql/soci-mysql.h"
 
 FDataBaseConnect::FDataBaseConnect()
 {
 	const std::string& ConnectionString = FDataBaseSettings::GetConnectionString();
-    static const std::string BackendName = "mysql";
 
     try
     {
-        // Create connection
-		Session = std::make_unique<soci::session>(BackendName, ConnectionString);
+        // Use static factory directly (avoids dlopen for libsoci_mysql.so)
+		Session = std::make_unique<soci::session>(soci::mysql, ConnectionString);
         bIsConnected = true;
     }
     catch (const soci::soci_error& e)
